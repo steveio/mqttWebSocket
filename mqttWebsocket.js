59a28becff98d87801d3d84c87588518cd2c02f4
steveio.com <info@steveio.com>
2020-04-18 21:18:36 +0100

    MongoDB aggregation to compute 3h moving average

--- mqttWebsocket.js
+++ mqttWebsocket.js
@@ -7,12 +7,31 @@ Demonstrates a Mosquito MQTT Client forwarding to Websocket
 Concept is to forward ESP8266 wifi Arduino sensor data from an MQTT server
 to a Websocket for consumption by web browser based real time graph client
 
+Includes MongoDB aggregation query to compute 3 hour simple moving average
+
 Usage:
   node mqttWSServer.js
 
-Requires mqtt, ws:
+Example Messaging in/out flow:
+
+  esp8266.out: [{"ts":1587244500,"tempC":19.6,"tempF":67.28,"h":65,"LDR":1016,"p":101656,"tc2":20.3,"a":4.563207}]
+  1 Listner(s) listening to mqttClient message event
+  [ [Function: sendMsg] ]
+  [ { ts: 1587244500,
+      tempC: 19.6,
+      tempF: 67.28,
+      h: 65,
+      LDR: 1016,
+      p: 101656,
+      tc2: 20.3,
+      a: 4.563207,
+      pAvg: 101305 } ]
+
+
+Requires mqtt, ws, mongodb:
   npm install mqtt
   npm install ws
+  npm install mongodb
 
 */
 
@@ -32,6 +51,19 @@ var auth = (mqtt_url.auth || ':').split(':');
 var url = "mqtt://" + mqtt_url.host;
 var mqtt_channel_in = "esp8266.in";
 var mqtt_channel_out = "esp8266.out";
+
+// setup mongoDB connection
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var dbname = "weather";
+var dbo = null;
+var pAvgInterval = 10800; // (secs) 3hour average
+var pAvg = 0;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  dbo = db.db(dbname);
+});
 
 
 //username: auth[0] + ":" + auth[0] if you are on a shared instance
@@ -71,13 +103,24 @@ mqttClient.on('message', function sendMsg(topic, message, packet) {
   console.log(eventListeners + " Listner(s) listening to mqttClient message event");
   console.log(mqttClient.rawListeners('message'));
 
+  // Query mongo DB - compute barometric air pressure 3 hour average
+  computeAvg();
+
+  // append avg to JSON message
+  if(message.toString().length > 1)
+  {
+    var jsonObj = JSON.parse(message.toString());
+    jsonObj[0]['pAvg'] = pAvg;
+    //console.log(jsonObj);
+    var jsonStr = JSON.stringify(jsonObj);
+  }
+
   wss.clients.forEach(function each(ws) {
     if (ws.isAlive === false) return ws.terminate();
-    ws.send(message+" ");
+    ws.send(jsonStr+" ");
   });
 
 });
-
 
 const interval = setInterval(function ping() {
   wss.clients.forEach(function each(ws) {
@@ -96,3 +139,26 @@ wss.on('close', function close() {
 wss.on('listening', function connection() {
   console.log('WS listening()');
 });
+
+/**
+MongoDB Aggregration Query:
+Query MongoDB to compute 3hour simple moving average barometric air pressure
+*/
+var computeAvg = function() {
+
+  var maxts = null;
+
+  dbo.collection('sensorData').find({},{"ts":1}).sort({ts:-1}).limit(1).map( function(d) { return d.ts; } ).toArray(function(err, result) {
+    if (err) throw err;
+    maxts = result[0];
+    //console.log(result[0]);
+  });
+
+  var query = dbo.collection('sensorData').aggregate([{ $match: {"ts":{$gte: maxts - pAvgInterval}} },{ $group: { _id : null,avg: { $avg: "$p" } } }]);
+
+  avg = query.toArray(function(err, result) {
+    if (err) throw err;
+    //console.log(result[0]['avg']);
+    pAvg = Math.trunc( result[0]['avg'] );
+  });
+}
